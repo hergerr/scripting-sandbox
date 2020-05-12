55abc978084632af6c289831c22107cccbcfbb51
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-05-12 17:15:24 +0200

    Added ex1

---
+++ lab4/ex02.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Napisz skrypt, który z zadanego miejsca systemu plików (parametr wywołania skryptu),
+# uwzględniając wszystkie możliwe podkatalogi, wypisze ścieżki do N (opcjonalny 
+# parametr wywołania skryptu; domyślnie 10) największych (pod względem rozmiaru w bajtach) 
+# plików. Jeśli kilka plików ma taki sam rozmiar, wybieramy dowolny z nich (bez znaczenia który).
+
+dir=$1
+
+# 10 jest domyslne
+number=${2:-10}
+
+find $dir -type f -printf '%s bytes. Path: %p\n' | sort -nr | head -$number
---
+++ lab4/make_env02.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p dir1/dir11/dir111
+cd dir1
+echo "jksdnkjasdnkjsdankjsadkds a sda dsa das dadiasndiasdninjisadisadniasd" > plik1.txt
+echo "sdadasda" > plik2.txt
+mkdir dir12
+cd dir12
+echo "sdanjdskankjsadnjksadnkjnsdajkndsakjndsakjndkjsandkjsandkjsanjkdajkdasnjkdnakjsdnsakjdnjkasndkjas" > plik3.txt
+cd ../dir11
+echo "abc" > plik4.txt
+echo "ndsandsjkandkjsandkjsanjkdsa" > plik5.txt
+cd dir111 
+echo "sdanjdskankjsadnjksadnkjnsdajkndsakjndsakjndkjsandkjsandkjsanjkdajkdasnjkdnakjsdnsakjdnjkasndkjas" > plik6.txt
